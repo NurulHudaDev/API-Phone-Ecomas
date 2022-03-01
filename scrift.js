9dc9046b0ae4448c3d360b5ed98f983c6a7a6850
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-03-01 21:25:03 +0300

    assignment-6

--- scrift.js
+++ scrift.js
@@ -12,6 +12,9 @@ const searchButton = () =>{
         input.value = '';
         mein.innerText = '';
     }
+    else if(data.data === 0){
+        error.innerText = 'please type text';
+    }
     else{
         fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(res => res.json())
@@ -54,15 +57,31 @@ const detailsButton = (id) =>{
 }
 
 const detailsDisply = (info) =>{
+    console.log(info);
     const details = document.getElementById('details');
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 25rem;">
     <img src="${info.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title"></h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <p><span class="fw-bold">Brand: </span>${info.brand}</p>
+      <p><span class="fw-bold">name: </span>${info.name}</p>
+      <p><span class="fw-bold">storage: </span>${info.mainFeatures.storage}</p>
+      <p><span class="fw-bold">chipSet: </span>${info.mainFeatures.chipSet}</p>
+      <p><span class="fw-bold">displaySize: </span>${info.mainFeatures.displaySize}</p>
+      <p><span class="fw-bold">memory: </span>${info.mainFeatures.memory}</p>
+      <p><span class="fw-bold">sensors: </span>${info.mainFeatures.sensors}</p>
+      <p class="fw-bold">others</p>
+     <div>
+        <p><span class="fw-bold">Bluetooth: </span>${info.others.Bluetooth}</p>
+        <p><span class="fw-bold">GPS: </span>${info.others.GPS}</p>
+        <p><span class="fw-bold">NFC: </span>${info.others.NFC}</p>
+        <p><span class="fw-bold">Radio: </span>${info.others.Radio}</p>
+        <p><span class="fw-bold">USB: </span>${info.others.USB}</p>
+        <p><span class="fw-bold">WLAN: </span>${info.others.WLAN}</p>
+        <p><span class="fw-bold">releaseDate: </span>${info.others.releaseDate}</p>
+        <p><span class="fw-bold">slug: </span>${info.others.slug}</p>
+     </div>
     </div>
   </div>
     `
