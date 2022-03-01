7f3a108115e824a9c06f8efd7d5ebdef0df9c564
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-03-01 18:54:53 +0300

    assignment-6

--- scrift.js
+++ scrift.js
@@ -36,11 +36,35 @@ const phoneDisply = (phones) =>{
             <div class="card-body">
             <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
             <p class="card-text">Bernd: ${phone.brand}</p>
-            <button type="button" class="btn btn-primary">Details</button>
+            <button onclick="detailsButton('${phone.slug}')" type="button" class="btn btn-primary">Details</button>
             </div>
             </div>
     `
     mein.appendChild(div);
     })
     
+}
+
+const detailsButton = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(res => res.json())
+    .then(data => detailsDisply(data.data))
+    details.innerHTML = ''
+
+}
+
+const detailsDisply = (info) =>{
+    const details = document.getElementById('details');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${info.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"></h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+    `
+    details.appendChild(div);
 }
